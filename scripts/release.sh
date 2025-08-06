e67a5b65250275a25c487516aad9dc89b573f35a
Awe Morris <awe@noctvm.io>
2025-08-07 03:42:56 +0900

    Fix the release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,7 +11,7 @@ else
     exit 1;
 fi    
 
-rm -rf dist NoctVM-latest.zip
+rm -rf dist Noct2D-latest.zip
 mkdir dist
 mkdir dist/windows
 mkdir dist/windows-arm64
@@ -31,8 +31,8 @@ cd build
 cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw-i686-toolchain.cmake
 cmake --build .
 cd ..
-cp build/noctvm.exe dist/windows/noctvm.exe
-cp build/noctcli.exe dist/windows/noctcli.exe
+cp build/noct2d.exe dist/windows/
+cp build/noct2dpack.exe dist/windows/
 rm -rf build
 
 #
@@ -45,8 +45,8 @@ cd build
 cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw-arm64-toolchain.cmake
 cmake --build .
 cd ..
-cp build/noctvm.exe dist/windows-arm64/noctvm.exe
-cp build/noctcli.exe dist/windows-arm64/noctcli.exe
+cp build/noct2d.exe dist/windows-arm64/
+cp build/noct2dpack.exe dist/windows-arm64/
 rm -rf build
 
 #
@@ -62,21 +62,21 @@ cmake .. -G Ninja
 cmake --build .
 
 # Sign & Notarize.
-codesign --timestamp --options runtime --entitlements ../resources/macos.entitlements --deep --force --sign "Developer ID Application" NoctVM.app
-ditto -c -k --sequesterRsrc --keepParent NoctVM.app NoctVM.zip
+codesign --timestamp --options runtime --entitlements ../resources/macos.entitlements --deep --force --sign "Developer ID Application" Noct2D.app
+ditto -c -k --sequesterRsrc --keepParent Noct2D.app Noct2D.zip
 source ../.credential
-xcrun notarytool submit NoctVM.zip --apple-id "$APPLE_ID" --team-id "$TEAM_ID" --password "$APP_PASSWORD" --wait
-xcrun stapler staple NoctVM.app
+xcrun notarytool submit Noct2D.zip --apple-id "$APPLE_ID" --team-id "$TEAM_ID" --password "$APP_PASSWORD" --wait
+xcrun stapler staple Noct2D.app
 
 # Pack.
 mkdir tmp
-cp -Rv NoctVM.app tmp/NoctVM.app
-hdiutil create -fs HFS+ -format UDBZ -srcfolder tmp -volname NoctVM NoctVM.dmg
-codesign --sign 'Developer ID Application' NoctVM.dmg
+cp -Rv Noct2D.app tmp/Noct2D.app
+hdiutil create -fs HFS+ -format UDBZ -srcfolder tmp -volname Noct2D Noct2D.dmg
+codesign --sign 'Developer ID Application' Noct2D.dmg
 
 cd ..
-cp build/NoctVM.dmg dist/macos/NoctVM.dmg
-cp build/noctcli dist/macos/noctcli
+cp build/Noct2D.dmg dist/macos/
+cp build/noct2dpack dist/macos/
 rm -rf build
 
 #
@@ -87,8 +87,8 @@ docker build --platform=linux/amd64 -t ubuntu22-x86_64 ./scripts
 mkdir build
 docker run --rm -it -v "$PWD":/src -w /src/build ubuntu22-x86_64 cmake .. -G Ninja
 docker run --rm -it -v "$PWD":/src -w /src/build ubuntu22-x86_64 cmake --build .
-cp build/noctvm dist/linux-x86_64/noctvm
-cp build/noctcli dist/linux-x86_64/noctcli
+cp build/noct2d dist/linux-x86_64/
+cp build/noct2dpack dist/linux-x86_64/
 rm -rf build
 
 #
@@ -99,8 +99,8 @@ docker build --platform=linux/arm64 -t ubuntu22-arm_64 ./scripts
 mkdir build
 docker run --rm -it -v "$PWD":/src -w /src/build ubuntu22-arm64 cmake .. -G Ninja
 docker run --rm -it -v "$PWD":/src -w /src/build ubuntu22-arm64 cmake --build .
-cp build/noctvm dist/linux-arm64/noctvm
-cp build/noctcli dist/linux-arm64/noctcli
+cp build/noct2d dist/linux-arm64/
+cp build/noct2dpack dist/linux-arm64/
 rm -rf build
 
 #
@@ -128,7 +128,7 @@ cd ../..
 #
 
 cd dist
-zip -r -9 NoctVM-all-platforms.zip windows windows-arm64 macos linux-x86_64 linux-arm64 unity
+zip -r -9 Noct2D-all-platforms.zip windows windows-arm64 macos linux-x86_64 linux-arm64 unity
 cd ..
 
 #
@@ -137,6 +137,6 @@ cd ..
 
 VERSION="$(date +%Y%m%d)-dev+$(git rev-parse --short HEAD)"
 gh release create $VERSION \
-   dist/NoctVM-all-platforms.zip \
-   --title "NoctVM $VERSION" \
-   --notes "This is the latest version of NoctVM!"
+   dist/Noct2D-all-platforms.zip \
+   --title "Noct2D $VERSION" \
+   --notes "This is the latest version of Noct2D!"
