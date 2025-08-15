3cd73769e2879a7d86cb13abde777cba93aaf566
Awe Morris <awe@noctvm.io>
2025-08-15 17:51:17 +0900

    Update the build system

--- scripts/build-unity-ps5.sh
+++ scripts/build-unity-ps5.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 rm -rf build-unity-ps5
-cmake -S . -B build-unity-ps5 -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/unity-ps5.cmake
+cmake -S . -B build-unity-ps5 -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/unity-ps5.cmake -DTARGET_UNITY=ON
 cmake --build build-unity-ps5
-
--- scripts/build-unity-win64.sh
+++ scripts/build-unity-win64.sh
@@ -2,5 +2,5 @@
 
 rm -rf build-unity-win64
 
-cmake -S . -B build-unity-win64 -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/unity-win64.cmake
+cmake -S . -B build-unity-win64 -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/unity-win64.cmake -DTARGET_UNITY=ON
 cmake --build build-unity-win64
