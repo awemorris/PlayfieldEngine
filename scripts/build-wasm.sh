79ade564c85d95d206162f6c1cdd7fd51f6c9e89
Awe Morris <awe@noctvm.io>
2025-08-15 17:55:30 +0900

    Update the build system

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -3,5 +3,5 @@
 set -eu
 
 rm -rf build-wasm
-emcmake cmake -S . -B build-wasm -DENABLE_I18N=OFF
+emcmake cmake -S . -B build-wasm -DENABLE_I18N=OFF -DTARGET_WASM=ON
 cmake --build build-wasm --parallel
