320bdbf55d31de2bd32deea167130530b98f24ec
Awe Morris <awe@noctvm.io>
2026-01-30 00:18:02 +0900

    Fix CI

--- external/StratoHAL/src/image.c
+++ external/StratoHAL/src/image.c
@@ -120,7 +120,7 @@ hal_create_image(
 		free(*img);
 		return false;
 	}
-#elif !defined(HAL_USE_UNITY)
+#elif !defined(HAL_TARGET_UNITY)
 	if (posix_memalign((void **)&pixels, 64, (size_t)w * (size_t)h * sizeof(hal_pixel_t)) != 0) {
 		hal_log_out_of_memory();
 		free(*img);
