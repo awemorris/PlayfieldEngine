c67654ed190e066f83b4a4a1b3c86f46d82f55b7
Awe Morris <awe@noctvm.io>
2026-05-08 17:45:01 +0900

    Fix pf_init_hook()

--- include/playfield/playfield.h
+++ include/playfield/playfield.h
@@ -900,11 +900,9 @@ pf_log_out_of_memory(void);
 /*
  * Special initialization function for derived engines.
  */
-#ifdef PF_USE_INITHOOK
 bool
 pf_init_hook(
 	int width,
 	int height);
-#endif
 
 #endif
