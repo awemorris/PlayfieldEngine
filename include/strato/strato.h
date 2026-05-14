4de054078be53f54267953996048e0b85ebcb9a2
Awe Morris <awe@noctvm.io>
2026-05-15 04:39:43 +0900

    Fix CI

--- include/strato/strato.h
+++ include/strato/strato.h
@@ -1685,7 +1685,6 @@ hal_bootstrap(
 				   nCmdShow);			\
 	}
 #define HAL_DEFINE_MAIN_CHAIN(chain_ptr, chain)			\
-#include <windows.h>
 int WINAPI WinMain(						\
 		HINSTANCE hInstance,				\
 		HINSTANCE hPrevInstance,			\
