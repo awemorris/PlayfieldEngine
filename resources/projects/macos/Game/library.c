8502c34fd85da7528aac873269e17cd467d67cc8
Awe Morris <awe@noctvm.io>
2026-05-16 14:51:25 +0900

    Update for HAL

--- resources/projects/macos/Game/library.c
+++ resources/projects/macos/Game/library.c
@@ -5,11 +5,10 @@
  *  $ playfield-aotc script1.ray script2.ray ...
  */
 
-#include <playfield/playfield.h>
-#include <noct/noct.h>
+struct rt_env;
 
 /* Please replace. */
-bool init_aot_code(NoctEnv *env)
+bool init_aot_code(struct rt_env *env)
 {
 	UNUSED_PARAMETER(env);
 
